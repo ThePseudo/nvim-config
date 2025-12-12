8503c39d73df22887ce837dd1c97a73482ba82f6
Andrea Calabrese <andrea.calabrese@amarulasolutions.com>
2025-12-12 09:02:07 +0100

    Add shortcut for fzf files
    Signed-off-by: Andrea Calabrese <andrea.calabrese@amarulasolutions.com>

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -1,12 +1,15 @@
 return {
-  "ibhagwan/fzf-lua",
-  -- optional for icon support
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  -- or if using mini.icons/mini.nvim
-  -- dependencies = { "nvim-mini/mini.icons" },
-  ---@module "fzf-lua"
-  ---@type fzf-lua.Config|{}
-  ---@diagnostics disable: missing-fields
-  opts = {}
-  ---@diagnostics enable: missing-fields
+	"ibhagwan/fzf-lua",
+	-- optional for icon support
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	-- or if using mini.icons/mini.nvim
+	-- dependencies = { "nvim-mini/mini.icons" },
+	---@module "fzf-lua"
+	---@type fzf-lua.Config|{}
+	---@diagnostics disable: missing-fields
+	opts = {},
+	---@diagnostics enable: missing-fields
+	keys = {
+		{'<Leader>f', '<cmd>FzfLua files<cr>'},
+	},
 }
