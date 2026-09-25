fb019cf244405703168c8e0e342cd39fbd961608
Andrea Calabrese <andrea.calabrese@amarulasolutions.com>
2026-09-25 14:22:14 +0200

    Add LSP support for clangd
    Signed-off-by: Andrea Calabrese <andrea.calabrese@amarulasolutions.com>

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -88,3 +88,16 @@ vim.keymap.set(
 	end,
 	{ silent = true, buffer = bufnr }
 )
+
+vim.lsp.config.clangd = {
+  cmd = {
+    'clangd',
+    '--clang-tidy',
+    '--background-index',
+    '--offset-encoding=utf-8',
+  },
+  root_markers = { '.clangd', 'compile_commands.json' },
+  filetypes = { 'c', 'cpp' },
+}
+
+vim.lsp.enable("clangd")
